6471be8577d992d914ebdacae233f06b6836795d
Bodo Junglas <junglas@objectcode.de>
2023-03-07 16:11:44 +0100

    Make tests work on windows

--- build.ps1
+++ build.ps1
@@ -33,3 +33,7 @@ Pop-Location
 Push-Location -Path "TO2-Test"
 msbuild -t:test -Property:Configuration=Release
 Pop-Location
+
+Push-Location -Path "KSP2Runtime-Test"
+msbuild -t:test -Property:Configuration=Release
+Pop-Location
