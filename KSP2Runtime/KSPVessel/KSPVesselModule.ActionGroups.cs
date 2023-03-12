eef9f38e76965c08e1138ccae406c777f1902be3
Bodo Junglas <junglas@objectcode.de>
2023-03-12 21:00:50 +0100

    Create bindings for custom action groups (addressing #10)

--- KSP2Runtime/KSPVessel/KSPVesselModule.ActionGroups.cs
+++ KSP2Runtime/KSPVessel/KSPVesselModule.ActionGroups.cs
@@ -37,6 +37,12 @@ namespace KontrolSystem.KSP.Runtime.KSPVessel {
             }
 
             [KSField]
+            public bool RadiatorPanels {
+                get => vessel.GetActionGroupState(KSPActionGroup.RadiatorPanels) == KSPActionGroupState.True;
+                set => vessel.SetActionGroup(KSPActionGroup.RadiatorPanels, value);
+            }
+
+            [KSField]
             public bool Gear {
                 get => vessel.GetActionGroupState(KSPActionGroup.Gear) == KSPActionGroupState.True;
                 set => vessel.SetActionGroup(KSPActionGroup.Gear, value);
@@ -52,6 +58,66 @@ namespace KontrolSystem.KSP.Runtime.KSPVessel {
             public bool Abort {
                 get => vessel.GetActionGroupState(KSPActionGroup.Abort) == KSPActionGroupState.True;
                 set => vessel.SetActionGroup(KSPActionGroup.Abort, value);
+            }
+            
+            [KSField]
+            public bool Custom1 {
+                get => vessel.GetActionGroupState(KSPActionGroup.Custom01) == KSPActionGroupState.True;
+                set => vessel.SetActionGroup(KSPActionGroup.Custom01, value);
+            }
+
+            [KSField]
+            public bool Custom2 {
+                get => vessel.GetActionGroupState(KSPActionGroup.Custom02) == KSPActionGroupState.True;
+                set => vessel.SetActionGroup(KSPActionGroup.Custom02, value);
+            }
+
+            [KSField]
+            public bool Custom3 {
+                get => vessel.GetActionGroupState(KSPActionGroup.Custom03) == KSPActionGroupState.True;
+                set => vessel.SetActionGroup(KSPActionGroup.Custom03, value);
+            }
+            
+            [KSField]
+            public bool Custom4 {
+                get => vessel.GetActionGroupState(KSPActionGroup.Custom04) == KSPActionGroupState.True;
+                set => vessel.SetActionGroup(KSPActionGroup.Custom04, value);
+            }
+            
+            [KSField]
+            public bool Custom5 {
+                get => vessel.GetActionGroupState(KSPActionGroup.Custom05) == KSPActionGroupState.True;
+                set => vessel.SetActionGroup(KSPActionGroup.Custom05, value);
+            }
+            
+            [KSField]
+            public bool Custom6 {
+                get => vessel.GetActionGroupState(KSPActionGroup.Custom06) == KSPActionGroupState.True;
+                set => vessel.SetActionGroup(KSPActionGroup.Custom06, value);
+            }
+            
+            [KSField]
+            public bool Custom7 {
+                get => vessel.GetActionGroupState(KSPActionGroup.Custom07) == KSPActionGroupState.True;
+                set => vessel.SetActionGroup(KSPActionGroup.Custom07, value);
+            }
+            
+            [KSField]
+            public bool Custom8 {
+                get => vessel.GetActionGroupState(KSPActionGroup.Custom08) == KSPActionGroupState.True;
+                set => vessel.SetActionGroup(KSPActionGroup.Custom08, value);
+            }
+            
+            [KSField]
+            public bool Custom9 {
+                get => vessel.GetActionGroupState(KSPActionGroup.Custom09) == KSPActionGroupState.True;
+                set => vessel.SetActionGroup(KSPActionGroup.Custom09, value);
+            }
+            
+            [KSField]
+            public bool Custom10 {
+                get => vessel.GetActionGroupState(KSPActionGroup.Custom10) == KSPActionGroupState.True;
+                set => vessel.SetActionGroup(KSPActionGroup.Custom10, value);
             }
         }
     }
