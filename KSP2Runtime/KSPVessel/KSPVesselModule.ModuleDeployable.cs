de521ed6d1bb319af87a1b018ec8fbe5468b6239
Charles Oliner <github.3yhhb@charlesoliner.com>
2023-03-15 23:05:09 -0400

    Added solar panel API
    Added solar panel API
    Added Deployable API

---
+++ KSP2Runtime/KSPVessel/KSPVesselModule.ModuleDeployable.cs
@@ -0,0 +1,32 @@
+﻿using KontrolSystem.TO2.Binding;
+using KSP.Modules;
+using KSP.Sim.impl;
+
+namespace KontrolSystem.KSP.Runtime.KSPVessel {
+    public partial class KSPVesselModule {
+        [KSClass("ModuleDeployable")]
+        public class ModuleDeployableAdapter {
+            private readonly PartComponent part;
+            private readonly Data_Deployable dataDeployable;
+
+            public ModuleDeployableAdapter(PartComponent part, Data_Deployable dataDeployable) {
+                UnityEngine.Debug.Log("Got part: " + part);
+                this.part = part;
+                this.dataDeployable = dataDeployable;
+            }
+
+            [KSField] public string PartName => part?.PartName ?? "Unknown";
+
+            [KSField] public string DeployState => dataDeployable.CurrentDeployState.GetValue().ToString();
+
+            [KSField] public bool Extendable => dataDeployable.extendable;
+
+            [KSField] public bool Retractable => dataDeployable.retractable;
+
+            [KSMethod]
+            public void SetExtended(bool extend) {
+                dataDeployable.toggleExtend.SetValue(extend);
+            }
+        }
+    }
+}
--- KSP2Runtime/KSPVessel/KSPVesselModule.ModuleSolarPanel.cs
+++ KSP2Runtime/KSPVessel/KSPVesselModule.ModuleSolarPanel.cs
@@ -1,6 +1,8 @@
 ﻿using KontrolSystem.TO2.Binding;
 using KSP.Modules;
 using KSP.Sim.impl;
+using static KontrolSystem.KSP.Runtime.KSPOrbit.KSPOrbitModule;
+using KontrolSystem.TO2.Runtime;
 
 namespace KontrolSystem.KSP.Runtime.KSPVessel {
     public partial class KSPVesselModule {
@@ -8,14 +10,23 @@ namespace KontrolSystem.KSP.Runtime.KSPVessel {
         public class ModuleSolarPanelAdapter {
             private readonly PartComponent part;
             private readonly Data_SolarPanel dataSolarPanel;
-            
+
             public ModuleSolarPanelAdapter(PartComponent part, Data_SolarPanel dataSolarPanel) {
+                UnityEngine.Debug.Log("Got part: " + part);
                 this.part = part;
                 this.dataSolarPanel = dataSolarPanel;
             }
 
-            [KSField]
-            public double EnergyFlow => dataSolarPanel.EnergyFlow.GetValue();
+            [KSField] public string PartName => part?.PartName ?? "Unknown";
+
+            [KSField] public double EnergyFlow => dataSolarPanel.EnergyFlow.GetValue();
+
+            [KSField] public Option<IBody> BlockingBody {
+                get {
+                    IBody body = KSPContext.CurrentContext.FindBody(dataSolarPanel.SimBlockingBody);
+                    return body != null ? Option.Some(body) : Option.None<IBody>();
+                }
+            }
         }
     }
 }
