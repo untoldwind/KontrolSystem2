3dfa404afc4537c4cb466ba6f98a872b5297ba51
Charles Oliner <github.3yhhb@charlesoliner.com>
2023-03-17 15:46:27 -0400

    Added decoupler and launch clamp APIs

---
+++ KSP2Runtime/KSPVessel/KSPVesselModule.ModuleDecoupler.cs
@@ -0,0 +1,45 @@
+﻿using KontrolSystem.TO2.Binding;
+using KSP.Modules;
+using KSP.Sim.impl;
+
+namespace KontrolSystem.KSP.Runtime.KSPVessel {
+    public partial class KSPVesselModule {
+        [KSClass("ModuleDecoupler")]
+
+        public class ModuleDecouplerAdapter {
+            private readonly PartComponent part;
+            private readonly Data_Decouple dataDecouple;
+
+            public ModuleDecouplerAdapter(PartComponent part, Data_Decouple dataDecouple) {
+                this.part = part;
+                this.dataDecouple = dataDecouple;
+            }
+
+            [KSField] public string PartName => part?.PartName ?? "Unknown";
+
+            [KSField]
+            public double EjectionImpulse {
+                get {
+                    return dataDecouple.EjectionImpulse.GetValue();
+                }
+                set {
+                    dataDecouple.EjectionImpulse.SetValue((float)value);
+                }
+            }
+
+            [KSField] public bool IsDecoupled => dataDecouple.isDecoupled.GetValue();
+
+            [KSMethod]
+            public bool Decouple() {
+
+                if (!KSPContext.CurrentContext.Game.SpaceSimulation.TryGetViewObject(part.SimulationObject,
+                       out var viewObject)) return false;
+
+                if (!viewObject.TryGetComponent<Module_Decouple>(out var moduleDecouple)) return false;
+
+                moduleDecouple.OnDecouple();
+                return dataDecouple.isDecoupled.GetValue();
+            }
+        }
+    }
+}
