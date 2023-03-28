721957f5e4312f176c350a5076e70f1b3adbf0f1
Charles Oliner <github.3yhhb@charlesoliner.com>
2023-03-27 22:16:20 -0400

    Added ability to type commands into the console window

---
+++ SpaceWarpMod/Utils/Expression.cs
@@ -0,0 +1,54 @@
+﻿using KontrolSystem.TO2.AST;
+using KontrolSystem.TO2.Parser;
+using KontrolSystem.TO2.Runtime;
+using KontrolSystem.TO2;
+using System.Linq;
+using KontrolSystem.Parsing;
+using KontrolSystem.KSP.Runtime.KSPConsole;
+using KontrolSystem.SpaceWarpMod.Core;
+
+namespace KontrolSystem.SpaceWarpMod.Utils {
+
+    public static class Expression {
+
+        public static object Run(string expression, KSPConsoleBuffer consoleBuffer) {
+            var result = TO2ParserREPL.REPLItems.Parse(expression);
+
+            var kspContext = new KSPContext(Mainframe.Instance.Game, consoleBuffer);
+            var registry = Mainframe.Instance.LastRegistry;
+            var context = new REPLContext(registry, kspContext);
+
+            var pollCount = 0;
+
+            try {
+                ContextHolder.CurrentContext.Value = kspContext;
+
+                foreach (var item in result.Where(i => !(i is IBlockItem))) {
+                    var future = item.Eval(context);
+                    var futureResult = future.PollValue();
+
+                    while (!futureResult.IsReady) {
+                        pollCount++;
+                        if (pollCount > 100) throw new System.Exception("No result after 100 tries");
+                        futureResult = future.PollValue();
+                    }
+                }
+
+                var mainBlock = new Block(result.OfType<IBlockItem>().ToList());
+                var mainFuture = mainBlock.Eval(context);
+                var mainFutureResult = mainFuture.PollValue();
+
+                while (!mainFutureResult.IsReady) {
+                    pollCount++;
+                    if (pollCount > 100) throw new System.Exception("No result after 100 tries");
+                    mainFutureResult = mainFuture.PollValue();
+                }
+
+                return mainFutureResult.value.Value;
+
+            } finally {
+                ContextHolder.CurrentContext.Value = null;
+            }
+        }
+    }
+}
