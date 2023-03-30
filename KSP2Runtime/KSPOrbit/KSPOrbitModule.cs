66c7e45dda97d5ace59e1a45ab51ff1a430f60b2
Bodo Junglas <junglas@objectcode.de>
2023-03-30 18:46:36 +0200

    Add function to obtain galactic celestial frame (addresses #46)

--- KSP2Runtime/KSPOrbit/KSPOrbitModule.cs
+++ KSP2Runtime/KSPOrbit/KSPOrbitModule.cs
@@ -1,14 +1,19 @@
-﻿using System.Linq;
-using KontrolSystem.TO2.Binding;
+﻿using KontrolSystem.TO2.Binding;
 using KontrolSystem.TO2.Runtime;
+using KSP.Sim;
 
 namespace KontrolSystem.KSP.Runtime.KSPOrbit {
     [KSModule("ksp::orbit")]
     public partial class KSPOrbitModule {
-        [KSFunction]
+        [KSFunction(Description = "Find a body by name.")]
         public static Result<IBody, string> FindBody(string name) {
             IBody body = KSPContext.CurrentContext.FindBody(name);
             return body != null ? Result.Ok<IBody, string>(body) : Result.Err<IBody, string>($"No such body '{name}'");
+        }
+
+        [KSFunction(Description = "Get the galactic celestial frame.")]
+        public static ITransformFrame GalacticOrigin() {
+            return KSPContext.CurrentContext.Game.UniverseModel.GalacticOrigin.celestialFrame;
         }
     }
 }
