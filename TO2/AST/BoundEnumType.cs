ecf1df9408aa730f0ef5f073aa35b53bef59ef66
Bodo Junglas <bjunglas@objectcode.de>
2023-04-02 08:54:36 +0200

    Support == and !=

--- TO2/AST/BoundEnumType.cs
+++ TO2/AST/BoundEnumType.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Reflection.Emit;
 using System.Text;
 using KontrolSystem.TO2.Generator;
+using KontrolSystem.TO2.Runtime;
 
 namespace KontrolSystem.TO2.AST {
     public class BoundEnumType : RealizedType {
@@ -9,9 +11,10 @@ namespace KontrolSystem.TO2.AST {
         public readonly string localName;
         private readonly string description;
         public readonly Type enumType;
+        public readonly OperatorCollection allowedSuffixOperators;
 
         public override Dictionary<string, IMethodInvokeFactory> DeclaredMethods { get; }
-        
+
         public BoundEnumType(string modulePrefix, string localName, string description, Type enumType) {
             this.modulePrefix = modulePrefix;
             this.localName = localName;
@@ -22,6 +25,16 @@ namespace KontrolSystem.TO2.AST {
                 { "to_string", new BoundMethodInvokeFactory("String representation of the number", 
                     true, () => BuiltinType.String, () => new List<RealizedParameter>(), false,
                     enumType, enumType.GetMethod("ToString", Type.EmptyTypes )) }
+            };
+            allowedSuffixOperators = new OperatorCollection() {
+                {
+                    Operator.Eq,
+                    new DirectOperatorEmitter(() => BuiltinType.Int, () => BuiltinType.Bool, REPLAny.ObjEq, OpCodes.Ceq)
+                }, {
+                    Operator.NotEq,
+                    new DirectOperatorEmitter(() => BuiltinType.Int, () => BuiltinType.Bool, REPLAny.ObjNeq, OpCodes.Ceq,
+                        OpCodes.Ldc_I4_0, OpCodes.Ceq)
+                }
             };
         }
 
@@ -44,6 +57,8 @@ namespace KontrolSystem.TO2.AST {
 
         public override string LocalName => localName;
 
+        public override IOperatorCollection AllowedSuffixOperators(ModuleContext context) => allowedSuffixOperators;
+        
         public override RealizedType UnderlyingType(ModuleContext context) => this;
 
         public override Type GeneratedType(ModuleContext context) => enumType;
