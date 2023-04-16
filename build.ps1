819c91799bc093e5a4c7b7cd11f1b230cb420bea
Bodo Junglas <bjunglas@objectcode.de>
2023-04-16 12:50:29 +0200

    Update build.ps1

--- build.ps1
+++ build.ps1
@@ -1,3 +1,4 @@
+
 Push-Location -Path "Parsing"
 msbuild -t:build -restore -Property:Configuration=Release
 Pop-Location
@@ -37,3 +38,8 @@ Pop-Location
 Push-Location -Path "KSP2Runtime-Test"
 msbuild -t:test -Property:Configuration=Release
 Pop-Location
+
+$KSP2_BASE_DIR = if($env:KSP2_BASE_DIR) { $env:KSP2_BASE_DIR } else { "C:\Program Files (x86)\Steam\steamapps\common\Kerbal Space Program 2" }
+if(Test-Path -Path $KSP2_BASE_DIR) {
+    Copy-Item -Path "./dist/*" -Destination $KSP2_BASE_DIR -Recurse -Force
+}
