7fda279203a7dfa3b1b6711b535aa10a1686b283
Bodo Junglas <junglas@objectcode.de>
2023-05-01 18:52:22 +0200

    Prepare next release

--- prepare_release.sh
+++ prepare_release.sh
@@ -10,3 +10,6 @@ zip -r KontrolSystem2-${version}.zip BepInEx
 
 gh release upload v${version} KontrolSystem2-${version}.zip
 gh release upload v${version} ../Tools/vscode/to2-syntax/to2-syntax-0.0.1.vsix
+gh release upload v${version} ../Tools/vscode/to2-syntax/to2-syntax-0.0.2.vsix
+mv ./Tools/vscode/to2-syntax/out/server.js ../Tools/vscode/to2-syntax/out/lsp-server.js 
+gh release upload v${version} ../Tools/vscode/to2-syntax/lsp-server.js
