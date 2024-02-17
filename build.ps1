e10fc415a05ddf038bdfce5444256b18ea36f632
Bodo Junglas <junglas@objectcode.de>
2024-02-17 20:32:01 +0100

    Update build.ps1
    Windows build

--- build.ps1
+++ build.ps1
@@ -1,43 +1,7 @@
 
-Push-Location -Path "Parsing"
-msbuild -t:build -restore -Property:Configuration=Release
-Pop-Location
+dotnet build -restore -Property:Configuration=Release
 
-Push-Location -Path "Parsing-Test"
-msbuild -t:build -restore -Property:Configuration=Release
-Pop-Location
-
-Push-Location -Path "TO2"
-msbuild -t:build -restore -Property:Configuration=Release
-Pop-Location
-
-Push-Location -Path "TO2-Test"
-msbuild -t:build -restore -Property:Configuration=Release
-Pop-Location
-
-Push-Location -Path "KSP2Runtime"
-msbuild -t:build -restore -Property:Configuration=Release
-Pop-Location
-
-Push-Location -Path "KSP2Runtime-Test"
-msbuild -t:build -restore -Property:Configuration=Release
-Pop-Location
-
-Push-Location -Path "SpaceWarpMod"
-msbuild -t:build -restore -Property:Configuration=Release
-Pop-Location
-
-Push-Location -Path "Parsing-Test"
-msbuild -t:test -Property:Configuration=Release
-Pop-Location
-
-Push-Location -Path "TO2-Test"
-msbuild -t:test -Property:Configuration=Release
-Pop-Location
-
-Push-Location -Path "KSP2Runtime-Test"
-msbuild -t:test -Property:Configuration=Release
-Pop-Location
+dotnet test --logger:"console;verbosity=detailed" -Property:Configuration=Release
 
 $KSP2_BASE_DIR = if($env:KSP2_BASE_DIR) { $env:KSP2_BASE_DIR } else { "C:\Program Files (x86)\Steam\steamapps\common\Kerbal Space Program 2" }
 if(Test-Path -Path $KSP2_BASE_DIR) {
