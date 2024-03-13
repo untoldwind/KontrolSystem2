9aea1ee3edc95b0acbdfd11ef41cfa62d90c28ea
Bodo Junglas <junglas@objectcode.de>
2024-03-13 23:30:14 +0100

    Use read-only span

--- Parsing/StringInput.cs
+++ Parsing/StringInput.cs
@@ -5,48 +5,40 @@ namespace KontrolSystem.Parsing;
 /// <summary>
 ///     Entire input for parsing is available as string.
 /// </summary>
-public readonly ref struct StringInput(string source, string sourceName, int position = 0, int line = 1, int column = 1) {
-    public char Current => source[position];
+public readonly ref struct StringInput(ReadOnlySpan<char> source, string sourceName, int position = 0, int line = 1, int column = 1) {
+    private readonly ReadOnlySpan<char> source = source;
+    
+    public char Current => source[0];
 
-    public int Available => source.Length - position;
+    public int Available => source.Length;
 
     public Position Position => new(sourceName, position, line, column);
 
     public int FindNext(Predicate<char> predicate) {
-        for (var p = position; p < source.Length; p++)
+        for (var p = 0; p < source.Length; p++)
             if (predicate(source[p]))
-                return p - position;
+                return p;
 
         return -1;
     }
 
-    public bool Match(string tag) {
-        var len = tag.Length;
-
-        if (source.Length - position < len) return false;
-
-        for (int i = 0; i < len; i++) {
-            if (tag[i] != source[position + i]) return false;
-        }
-
-        return true;
-    }
+    public bool Match(ReadOnlySpan<char> tag) => source.StartsWith(tag);
 
     public string Take(int count) {
         if (count == 0) return "";
-        if (count + position > source.Length) throw new InvalidOperationException("Advance beyond eof");
+        if (count > source.Length) throw new InvalidOperationException("Advance beyond eof");
 
-        return source.Substring(position, count);
+        return source.Slice(0, count).ToString();
     }
 
     public StringInput Advance(int count) {
         if (count == 0) return this;
-        if (count + position > source.Length) throw new InvalidOperationException("Advance beyond eof");
+        if (count > source.Length) throw new InvalidOperationException("Advance beyond eof");
 
         var nextLine = line;
         var nextColumn = column;
 
-        for (var p = position; p < position + count; p++) {
+        for (var p = 0; p < count; p++) {
             nextColumn++;
             if (source[p] == '\n') {
                 nextLine++;
@@ -54,10 +46,10 @@ public readonly ref struct StringInput(string source, string sourceName, int pos
             }
         }
 
-        return new StringInput(source, sourceName, position + count, nextLine, nextColumn);
+        return new StringInput(source.Slice(count), sourceName, position + count, nextLine, nextColumn);
     }
 
     public override string ToString() {
-        return source.Substring(position);
+        return source.ToString();
     }
 }
