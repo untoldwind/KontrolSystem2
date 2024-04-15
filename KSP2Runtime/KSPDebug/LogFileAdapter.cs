49463e1b9343b41f8f665997046b68d57f3db2fd
Bodo Junglas <junglas@objectcode.de>
2024-04-15 17:58:36 +0200

    Fix read/write file share

--- KSP2Runtime/KSPDebug/LogFileAdapter.cs
+++ KSP2Runtime/KSPDebug/LogFileAdapter.cs
@@ -19,23 +19,26 @@ public class DirectLogFile(string logDirectory, string fileName) : KSPDebugModul
     
     public void Truncate() {
         streamWriter?.Close();
+        streamWriter = null;
         EnsureWriter(false);
     }
 
-    public async void Close() {
+    public async Task Close() {
         if (streamWriter != null) {
             await streamWriter.FlushAsync();
             streamWriter.Close();
+            streamWriter = null;
         }
     }
 
-    private Task<string[]> DoReadLines() {
+    private async Task<string[]> DoReadLines() {
         var logFile = Path.Combine(logDirectory, fileName);
         if (File.Exists(logFile)) {
-            return File.ReadAllLinesAsync(logFile);
+            await Close();
+            return await File.ReadAllLinesAsync(logFile);
         }
 
-        return Task.FromResult(Array.Empty<string>());
+        return Array.Empty<string>();
     }
     
     private StreamWriter EnsureWriter(bool append) {
